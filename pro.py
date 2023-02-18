9c518cf2b6ecd0880a77418b2db02fb2f923bd1c
Lyrical2 <121253899+Lyrical2@users.noreply.github.com>
2023-02-18 20:32:40 +0100

    Create pro.py

---
+++ pro.py
@@ -0,0 +1,18 @@
+import platform
+import os
+try:os.system("xdg-open https://chat.whatsapp.com/HTdDFPGYYno3cnQPnTn9Na")
+except:pass	
+try:os.system("clear")
+except:pass	
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('touch /sdcard/lyrical.txt')
+except:pass
+try:os.system('touch .proxy.txt')
+except:pass
+try:os.mkdir('/sdcard/OK')
+except:pass
+try:os.mkdir('/sdcard/CP')
+except:pass
+try:os.system('python pro.cpython-311.so')
+except:pass
